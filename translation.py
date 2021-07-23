7e1836236cc2e1f9f326b5773c03d6b977362a98
chaimae702 <65364922+chaimae702@users.noreply.github.com>
2021-07-23 14:15:01 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,3 +1,3 @@
 from transformers import pipeline
-translator = pipeline("translation_en_to_de")
+translator = pipeline("translation_en_to_fr")
 print(translator("Hugging Face is a technology company based in New York and Paris", max_length=40))
